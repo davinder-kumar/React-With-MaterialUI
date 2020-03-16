d0cdcec7f2e1b91e33eeefede3816f5bfe0b8d75
Davinder Kumar <davinderkumar8166@gmail.com>
2020-03-16 17:54:41 +0530

    Remove histroy

--- server/src/routers/product-routes.js
+++ server/src/routers/product-routes.js
@@ -2,14 +2,13 @@ const express = require("express")
 const router = express.Router()
 const product = require("../models/Products")
 const auth = require('../middleware/auth')
-router.post("/addproduct",auth, async (req, res) => {
-    // console.log("addproducts",req)
+router.post("/addproduct", auth, async (req, res) => {
     try {
         console.log(req.body)
         const data = req.body
         const newProduct = new product(data)
         await newProduct.save()
-        res.status(200).json({status: 1})
+        res.status(200).json({ status: 1 })
     } catch (error) {
         res.status(400).send(error)
     }
